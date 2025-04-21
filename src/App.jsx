0828551094a87dcdce79d3885229c68ffa7b6496
asharaam1 <lohanaasharaam1@gmail.com>
2025-04-21 23:45:25 +0500

    sweetalert2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import './App.css'
+import Swal from 'sweetalert2'
 
 const App = () => {
 
@@ -21,32 +22,40 @@ const App = () => {
     setList(res.data.values);
   }
 
+  function myAlert() {
+    Swal.fire({
+      title: "Item Added",
+      icon: "success",
+      draggable: true
+    });
+  }
+
   return (
     <>
       <div>
         <h1 className=''>google sheet api fetch</h1>
         <div className='flex flex-wrap gap-4 background p-10'>
-          {list.map((item, index)=>{
-            return(
-          <div className="card bg-base-100 w-96 shadow-sm">
-            <figure>
-              <img
-                src={item[1]}
-                alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                {item[0]}
-                <div className="badge badge-secondary">NEW</div>
-              </h2>
-              <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-              <div className="card-actions justify-end">
-                <div className="badge badge-outline">Fashion</div>
-                <div className="badge badge-outline">Products</div>
+          {list.map((item, index) => {
+            return (
+              <div className="card bg-base-100 w-96 shadow-sm" key={index}>
+                <figure>
+                  <img
+                    src={item[1]}
+                    alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">
+                    {item[0]}
+                    <div className="badge badge-secondary">NEW</div>
+                  </h2>
+                  <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                  <div className="card-actions justify-end">
+                    <div className="badge badge-outline mybtn" onClick={myAlert}>Add to Cart</div>
+                    <div className="badge badge-outline mybtn">View</div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          )
+            )
           })}
         </div>
       </div>
